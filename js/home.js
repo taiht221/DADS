93fb6e114db276353b4b6dca9191c76cf49dd1b9
Tai Dentsu <tai.huynh1@iprospect.com>
2022-05-04 17:16:39 +0700

    reponsive web

--- js/home.js
+++ js/home.js
@@ -14,6 +14,20 @@ import '../scss/pages/home.scss'
     speed: 500,
     loop: true,
     loopFillGroupWithBlank: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+      },
+    },
   })
 
   function toggleNav() {
