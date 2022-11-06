b784dadc9e074f83332cb2ac825304d662b38081
Tai Merkle <taiht221@gmail.com>
2022-11-06 11:00:02 +0700

    update layout presale

--- js/home.js
+++ js/home.js
@@ -69,3 +69,15 @@ import '../scss/pages/home.scss'
     }
   }, 1000)
 })()
+
+document.getElementById('copyButton').addEventListener('click', () => {
+  // Get the text field
+  var copyText = document.getElementById('copyInput')
+
+  // Select the text field
+  copyText.select()
+  copyText.setSelectionRange(0, 99999) // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value)
+})
